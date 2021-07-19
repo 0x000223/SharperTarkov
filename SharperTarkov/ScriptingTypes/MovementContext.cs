64b69d679f24b08c03984397359bfcc4b2e70d76
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:06:11 +0300

    Add movement context file

---
+++ SharperTarkov/ScriptingTypes/MovementContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using SharpMemory.Ioctl;
+
+namespace SharperTarkov.ScriptingTypes
+{
+    public class MovementContext
+    {
+        public MovementContext(ulong address)
+        {
+            Address = address;
+        }
+
+        public ulong Address { get; }
+
+        public Vector2 Rotation
+        {
+            get => Memory.Read<Vector2>(Address + Offsets.MovementContext.Rotation);
+
+            set => Memory.Write(Address + Offsets.MovementContext.Rotation, value);
+        }
+    }
+}
