b49e345de3562ce824cf84ee92b16a00aa306533
Max Penkov <hello@maxpenkov.com>
2021-07-19 13:14:00 +0300

    Update player body class - cleanup

--- SharperTarkov/ScriptingTypes/PlayerBody.cs
+++ SharperTarkov/ScriptingTypes/PlayerBody.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 using System.Collections.Generic;
 
-using SharperTarkov.UnityEngineTypes;
-
 using SharpMemory.Ioctl;
+using SharperTarkov.UnityEngineTypes;
 
 namespace SharperTarkov.ScriptingTypes
 {
@@ -12,65 +12,58 @@ namespace SharperTarkov.ScriptingTypes
     {
         public PlayerBody(ulong address) : base(address)
         {
-            var skeleton = Memory.Read<ulong>(Address + Offsets.PlayerBody.SkeletonRootJoint);
-
-            BoneTransforms = MemoryHelper.ReadList<Transform>(skeleton + Offsets.Skeleton.Values);
+            Initialize(address);
         }
 
-        public Transform this[EBone bone] => BoneTransforms[(int) bone] ?? new Transform();
-
-        public List<Transform> BoneTransforms { get; }
-
-        public List<Vector3> BonePositions { get; set; }
-
-        public static readonly int[] BoneIndicies = 
+        private void Initialize(ulong address)
         {
+            if (address == 0)
+            {
+                return;
+            }
+
+            var skeleton = Memory.Read<ulong>(address + Offsets.PlayerBody.SkeletonRootJoint);
+
+            var scriptingTransforms = MemoryHelper.ReadList<ulong>(skeleton + Offsets.Skeleton.Values, BoneIndices);
+
+            Transforms = scriptingTransforms.Select(addr => new Transform(Memory.Read<ulong>(addr + 0x10))).ToList();
+
+            Positions = new List<Vector3>(Transforms.Select(t => t.GetPosition()));
+        }
+
+        public Vector3 this[EBone bone] => Positions[(int) bone];
+
+        public List<Transform> Transforms { get; private set; }
+
+        public List<Vector3> Positions { get; private set; }
+
+        private static readonly int[] BoneIndices =
+        {
+            // Indices of specific bones I need from skeleton transform list
             13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 29, 36, 37, 90, 91,
             92, 93, 111, 112, 113, 114, 132, 133
         };
 
-        public static readonly int[] BoneLinkIndicies =
+        public static readonly int[] BoneLinkIndices =
         {
-            133, 132, 132, 29, 29, 14, 111, 132, 112, 111,
-            113, 112, 114, 113, 90, 132, 91, 90, 92, 91, 93, 92,
-            20, 14, 21, 20, 22, 21, 23, 22, 14, 15, 15, 16, 16, 17, 17, 18
+            // Indices of Transforms list
+            22, 21, 21, 2, 17, 21, 18, 17, 19, 18,
+            20, 19, 13, 21, 14, 13, 15, 14, 16, 15,
+            6, 2, 7, 6, 8, 7, 9, 8, 2, 3, 3, 4, 4, 5
         };
-
-        //PlayerBones = new()
-        //{
-        //    Root = values[13],
-        //    Pelvis = values[14],
-        //    LeftThigh1 = values[15],
-        //    LeftThigh2 = values[16],
-        //    LeftCalf = values[17],
-        //    LeftFoot = values[18],
-        //    RightThigh1 = values[20],
-        //    RightThigh2 = values[21],
-        //    RightCalf = values[22],
-        //    RightFoot = values[23],
-        //    Spine1 = values[29],
-        //    Spine2 = values[36],
-        //    Spine3 = values[37],
-        //    LeftUpperArm = values[90],
-        //    LeftForearm1 = values[91],
-        //    LeftForearm2 = values[92],
-        //    LeftForearm3 = values[93],
-        //    RightUpperArm = values[111],
-        //    RightForearm1 = values[112],
-        //    RightForearm2 = values[113],
-        //    RightForearm3 = values[114],
-        //    Neck = values[132],
-        //    Head = values[133]
-        //};
     }
 
     public enum EBone
     {
-        Root = 13,
-        Pelvis = 14,
-        LeftFoot = 18,
-        RightFoot = 23,
-        Neck = 132,
-        Head = 133
+        Root = 0,
+        Pelvis = 1,
+        LeftTigh1 = 2,
+        LeftFoot = 5,
+        RightFoot = 9,
+        Spine1 = 10,
+        RightUpperArm = 17,
+        RightForearm1 = 18,
+        Neck = 21,
+        Head = 22
     }
 }
