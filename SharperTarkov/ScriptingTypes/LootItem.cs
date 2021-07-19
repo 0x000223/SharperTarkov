5ef78df053ecb8223c82205cb8e1302973d636b2
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:05:45 +0300

    Add loot item file

---
+++ SharperTarkov/ScriptingTypes/LootItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SharpMemory.Ioctl;
+
+namespace SharperTarkov.ScriptingTypes
+{
+    public class LootItem
+    {
+        public LootItem() { }
+
+        public LootItem(ulong address)
+        {
+            if (address == 0)
+            {
+                return;
+            }
+
+            Address = address;
+
+            ItemName = MemoryHelper.ReadWideString(address + Offsets.LootItem.Name);
+
+            Item = new Item(Memory.Read<ulong>(address + Offsets.LootItem.Item));
+        }
+
+        public ulong Address { get; }
+
+        public string ItemName { get; }
+
+        public Item Item { get; }
+    }
+}
