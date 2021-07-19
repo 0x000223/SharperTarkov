b33cc1093ba76e530c98d7523f428713bdb2f139
Max Penkov <hello@maxpenkov.com>
2021-07-19 13:27:09 +0300

    Update player class

--- SharperTarkov/ScriptingTypes/Player.cs
+++ SharperTarkov/ScriptingTypes/Player.cs
@@ -1,35 +1,96 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using SharpMemory.Ioctl;
+
 using SharperTarkov.UnityEngineTypes;
 
 namespace SharperTarkov.ScriptingTypes
 {
     public class Player : MonoBehaviour
     {
+        public Player() { }
+
         public Player(ulong address) : base(address)
         {
-            Profile = new PlayerProfile(Memory.Read<ulong>(Address + Offsets.Player.PlayerProfile));
-
-            PlayerBody = new PlayerBody(Memory.Read<ulong>(Address + Offsets.Player.PlayerBody));
-
-            Physical = new PlayerPhysical(Memory.Read<ulong>(Address + Offsets.Player.PlayerPhysical));
-
-            HealthController = new HealthController(Memory.Read<ulong>(Address + Offsets.Player.HealthController));
+            Initialize(address);
         }
 
-        public PlayerProfile Profile { get; }
+        public PlayerProfile Profile { get; private set; }
 
-        public PlayerBody PlayerBody { get; }
+        public PlayerBody Body { get; private set; }
 
-        public PlayerPhysical Physical { get; }
+        public PlayerPhysical Physical { get; private set; }
 
-        public HealthController HealthController { get; }
+        public HealthController HealthController { get; private set; }
 
-        public float Health => HealthController.BodyParts.Sum(bodypart => bodypart.Current);
+        public ProceduralWeaponAnimation ProcWepAnim { get; private set; }
+
+        public MovementContext MovementContext { get; private set; }
+
+        public float Health { get; private set; }
+
+        public bool IsLocalPlayer { get; private set; }
+
+        public bool IsSavage { get; private set; }
+
+        public bool IsSavagePlayer { get; private set; }
 
         public string PlayerName => Profile.Nickname;
 
-        public bool IsSavage => Profile.Side == EPlayerSide.Savage;
+        public int PlayerLevel => Profile.Level;
+
+        public ulong HandsController => Memory.Read<ulong>(Address + Offsets.Player.HandsController);
+
+        private void Initialize(ulong address)
+        {
+            if (address == 0)
+            {
+                return;
+            }
+
+            Profile = new PlayerProfile(Memory.Read<ulong>(address + Offsets.Player.PlayerProfile));
+
+            Body = new PlayerBody(Memory.Read<ulong>(address + Offsets.Player.PlayerBody));
+
+            Physical = new PlayerPhysical(Memory.Read<ulong>(address + Offsets.Player.PlayerPhysical));
+
+            HealthController = new HealthController(Memory.Read<ulong>(address + Offsets.Player.HealthController));
+
+            ProcWepAnim = new ProceduralWeaponAnimation(Memory.Read<ulong>(address + Offsets.Player.ProceduralWeaponAnimation));
+
+            MovementContext = new MovementContext(Memory.Read<ulong>(Address + Offsets.Player.MovementContext));
+
+            IsLocalPlayer = Memory.Read<bool>(address + Offsets.Player.IsLocalPlayer);
+
+            IsSavage = Profile.Side == EPlayerSide.Savage;
+
+            IsSavagePlayer = IsSavage && PlayerLevel > 0;
+        }
+
+        public void Update()
+        {
+            Health = HealthController.BodyParts.Sum(bodypart => bodypart.Current);
+        }
+
+        public float Distance(Player other) => Vector3.Distance(Body[EBone.Pelvis], other.Body[EBone.Pelvis]);
+
+        public Transform GetFireport()
+        {
+            var playerBones = Memory.Read<ulong>(Address + Offsets.Player.PlayerBones);
+
+            var bifacialTransform = Memory.Read<ulong>(playerBones + Offsets.PlayerBones.Fireport);
+
+            var scriptingTransform = Memory.Read<ulong>(bifacialTransform + Offsets.BifacialTransform.Original);
+
+            var transformAddress = Memory.Read<ulong>(scriptingTransform + 0x10);
+
+            if (transformAddress == 0)
+            {
+                return new Transform();
+            }
+
+            return new Transform(transformAddress);
+        }
     }
 }
