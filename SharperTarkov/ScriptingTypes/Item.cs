68c3eba7f26d626dc2adc05f4b363db18c634602
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:05:21 +0300

    Add item file

---
+++ SharperTarkov/ScriptingTypes/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using SharpMemory.Ioctl;
+
+namespace SharperTarkov.ScriptingTypes
+{
+    public class Item
+    {
+        public Item() { }
+
+        public Item(ulong address)
+        {
+            if (address == 0)
+            {
+                return;
+            }
+
+            Address = address;
+
+            SpawnedInSession = Memory.Read<bool>(address + Offsets.Item.SpawnedInSession);
+
+            Template = new ItemTemplate(Memory.Read<ulong>(address + Offsets.Item.ItemTemplate));
+        }
+
+        public ulong Address { get; }
+
+        public bool SpawnedInSession { get; }
+
+        public ItemTemplate Template { get; }
+    }
+}
