cb9aea8cb6e3f7f22d522f385d78df68dfbb5b0c
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:05:11 +0300

    Add grenade template file

---
+++ SharperTarkov/ScriptingTypes/GrenadeTemplate.cs
@@ -0,0 +1,22 @@
+﻿using SharpMemory.Ioctl;
+
+namespace SharperTarkov.ScriptingTypes
+{
+    public class GrenadeTemplate : ItemTemplate
+    {
+        public GrenadeTemplate(ulong address) : base(address)
+        {
+            GrenadeType = (EThrowWeaponType) Memory.Read<int>(address + Offsets.GrenadeTemplate.ThrowType);
+
+            ExplostionDelay = Memory.Read<float>(address + Offsets.GrenadeTemplate.ExplosionDelay);
+
+            Strength = Memory.Read<float>(address + Offsets.GrenadeTemplate.Strength);
+        }
+
+        public EThrowWeaponType GrenadeType { get; }
+
+        public float ExplostionDelay { get; }
+
+        public float Strength { get; }
+    }
+}
