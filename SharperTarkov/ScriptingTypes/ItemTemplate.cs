0c03040ecf1a6da6a950f45cc443ab01a3da099d
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:05:30 +0300

    Add item template file

---
+++ SharperTarkov/ScriptingTypes/ItemTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace SharperTarkov.ScriptingTypes
+{
+    public class ItemTemplate
+    {
+        public ItemTemplate(ulong address)
+        {
+            Address = address;
+
+            ShortName = MemoryHelper.ReadWideString(address + Offsets.ItemTemplate.ShortName);
+        }
+
+        public ulong Address { get; }
+
+        public string ShortName { get; }
+    }
+}
