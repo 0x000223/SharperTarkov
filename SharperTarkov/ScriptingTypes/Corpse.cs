8a99c67301a40a341b7312b9d76f996a525f9926
Max Penkov <hello@maxpenkov.com>
2021-07-19 14:04:42 +0300

    Add corpse file

---
+++ SharperTarkov/ScriptingTypes/Corpse.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using SharperTarkov.UnityEngineTypes;
+using SharpMemory.Ioctl;
+
+namespace SharperTarkov.ScriptingTypes
+{
+    public class Corpse : LootItem
+    {
+        public Corpse(ulong address) : base(address)
+        {
+            PlayerSide = (EPlayerSide) Memory.Read<int>(address + Offsets.Corpse.PlayerSide);
+
+            var scriptedTransform = Memory.Read<ulong>(address + Offsets.Corpse.PelvisTransform);
+
+            var pelvis = new Transform(Memory.Read<ulong>(scriptedTransform + 0x10));
+
+            Position = pelvis.GetPosition();
+        }
+
+        public EPlayerSide PlayerSide { get; }
+
+        public Vector3 Position { get; }
+    }
+}
